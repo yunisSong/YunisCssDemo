9c585a287937b1d0a969b77b52e41bcc539f67d4
三十一 <yunisSong@users.noreply.github.com>
2022-06-05 00:29:53 +0800

    搜索框、banner
    搜索框、banner

--- jingdong/jd/src/App.vue
+++ jingdong/jd/src/App.vue
@@ -6,9 +6,17 @@
   <div class="wrapper">
     <div class="position">
       <span class="iconfont position__adress">&#xe7f1;</span>
-      <span class="">圣诞快乐sadfsadf啥撒打发士大夫撒打发斯蒂芬地方撒的发生大方姐</span>
+      <span class="">深圳市南山区高西南11道国实大厦</span>
       <span class="iconfont position__notice">&#xe60b;</span>
     </div>
+    <div class="search">
+      <span class="iconfont">&#xe6ac;</span>
+      <input class="search__input" type="text" placeholder="撒打发士大夫撒打发斯蒂芬地方撒">
+    </div>
+    <div class="banner">
+      <img class="banner__img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg" alt="" >
+    </div>
+    <div>12222</div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -32,6 +40,7 @@
 
 <style lang="scss">
 @import './style/variable.scss';
+@import './style/mixins.scss';
 .wrapper {
   position: absolute;
   left: 0;
@@ -43,19 +52,54 @@
 
 }
 .position {
-  background-color: gainsboro;
   position: relative;
-  line-height: .16rem;
+  line-height: .22rem;
+  padding: .16rem;
+  padding-left: 0;
+  font-size: .16rem;
+  color: $content-fontcolor;
+  @include ellipsis;
 
   &__adress {
-    line-height: .16rem;
     margin-right: .05rem;
+    font-size: .2rem;
   }
   &__notice {
     position: absolute;
     right: 0;
-    top: 0;
-    line-height: .16rem;
+    font-size: .2rem;
+  }
+}
+
+.search {
+  margin-bottom: .16rem;
+  line-height: .32rem;
+  background-color: #f5f5f5;
+  border-radius: .16rem;
+  padding: 0 .16rem;
+  font-size: .14rem;
+  color: #b7b7b7;
+  .iconfont {
+    position: relative;
+    top: .02rem;
+    font-size: .2rem;
+  }
+  &__input {
+    border: none;
+    outline: none;
+    background-color: #f5f5f5;
+    width: calc(100% - (.2rem + .16rem));
+  }
+}
+
+.banner {
+  // 下面这三行是给 图片一个默认的占位高度，防止图片下面的视图，在图片未加载出来之前出现页面跳动的情况。
+  // 其中  height: 0;overflow: hidden;padding-bottom: 25.4%; 的意思就是给定一个高度为宽度 25.4%的约束。
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
   }
 }
 
