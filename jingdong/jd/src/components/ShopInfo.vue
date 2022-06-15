aae59cac71d47b53f4ad2605ceca76f9ba41b0c3
yunissong <332963965@qq.com>
2022-06-15 09:23:00 +0800

    shopinfo component

---
+++ jingdong/jd/src/components/ShopInfo.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+      class="shop"
+    >
+      <div class="shop__img"><img :src="item.imageURL" alt=""></div>
+      <div class="shop__content"
+      :class="{'shop__content':true, 'shop__content--bordered': hiddenBorder ? false : true}"
+      >
+        <div class="shop__content__title">{{ item.title }}</div>
+        <div class="shop__content__tags">
+          <span
+            class="shop__content__tags__tag"
+            v-for="(innerItem, index) in item.tags"
+            :key="index"
+          >{{ innerItem }}</span>
+
+        </div>
+        <div class="shop__content__highlight">{{ item.description }}</div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ShopInfo',
+  props: ['item', 'hiddenBorder']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variable.scss';
+.shop {
+  width: 100%;
+  display: flex;
+  padding-top: .12rem;
+  &__img {
+
+    width: .64rem;
+    height: .64rem;
+    margin-right: .08rem;
+    margin-top: .06rem;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  &__content {
+    flex: 1;
+    &--bordered {
+      border-bottom: 1px solid $bg-layer-color;
+    }
+    &__title {
+      font-size: .14rem;
+      font-weight: normal;
+      color: $content-fontcolor;
+    }
+    &__tags {
+      display: flex;
+      // justify-content: space-between;
+      margin-top: .08rem ;
+      font-size: .12rem;
+      font-weight: normal;
+      color: $content-fontcolor;
+      &__tag {
+        flex:1;
+      }
+    }
+    &__highlight {
+      margin-top: .08rem;
+      margin-bottom: .08rem;
+      font-size: .12rem;
+      font-weight: normal;
+      color: red;
+    }
+  }
+}
+</style>
--- jingdong/jd/src/views/Home/Nearby.vue
+++ jingdong/jd/src/views/Home/Nearby.vue
@@ -6,26 +6,16 @@
       v-for='item in nearbyList'
       :key="item.id"
     >
-      <div class="nearby__item__img"><img :src="item.imageURL" alt=""></div>
-      <div class="nearby__item__content">
-        <div class="nearby__item__content__title">{{ item.title }}</div>
-        <div class="nearby__item__content__tags">
-          <span
-            class="nearby__item__content__tag"
-            v-for="(innerItem, index) in item.tags"
-            :key="index"
-          >{{ innerItem }}</span>
-
-        </div>
-        <div class="nearby__item__content__highlight">{{ item.description }}</div>
-      </div>
+      <ShopInfoVue :item="item" />
     </div>
   </div>
 </template>
 
 <script>
+import ShopInfoVue from '../../components/ShopInfo.vue'
 export default {
   name: 'Nearby',
+  components: { ShopInfoVue },
   setup () {
     const nearbyList = [
       {
@@ -64,55 +54,10 @@ export default {
 
 <style lang="scss">
 @import '../../style/variable.scss';
-
-.nearby {
-  h3 {
-    font-size: .18rem;
-    font-weight: normal;
-    color: $content-fontcolor;
-    margin: .12rem 0;
-  }
-  &__item {
-    display: flex;
-    padding-top: .12rem;
-    &__img {
-
-      width: .64rem;
-      height: .64rem;
-      margin-right: .08rem;
-      margin-top: .06rem;
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-    &__content {
-      flex: 1;
-      border-bottom: 1px solid $bg-layer-color;
-
-      &__title {
-        font-size: .14rem;
-        font-weight: normal;
-        color: $content-fontcolor;
-      }
-      &__tags {
-        display: flex;
-        margin-top: .08rem;
-        font-size: .12rem;
-        font-weight: normal;
-        color: $content-fontcolor;
-        .nearby__item__content__tag {
-          flex:1;
-        }
-      }
-      &__highlight {
-        margin-top: .08rem;
-        margin-bottom: .08rem;
-        font-size: .12rem;
-        font-weight: normal;
-        color: red;
-      }
-    }
-  }
+h3 {
+  font-size: .18rem;
+  font-weight: normal;
+  color: $content-fontcolor;
+  margin: .12rem 0;
 }
 </style>
